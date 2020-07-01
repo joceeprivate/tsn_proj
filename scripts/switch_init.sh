0cf299c3a5bbf60e5924ecf374a9f7936550bc3e
joceeprivate <jocee199275@163.com>
2020-07-01 12:54:38 +0800

    switch_init funcs updated

--- scripts/switch_init.sh
+++ scripts/switch_init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-    echo "Input board Number (0-9)."
+if [ $# -ne 3 ]; then
+    echo "Wrong No. of input parameters. Should be 3."
     exit
 fi
 
@@ -20,12 +20,57 @@ if [ $1 -ge 0 ] && [ $1 -le 9 ]; then
     ifconfig ep up
     ifconfig eth1 up
     ifconfig eth2 up
-    devmem 0x43c3800c 32 0x350001"$1"0
-    devmem 0x43c38010 32 0xf000a
+    devmem 0x8007800c 32 0x350001"$1"0
+    devmem 0x80078010 32 0xf000a
     echo "##### Board $1 IP & MAC config done. #####"
 else
     echo "Wrong board num. Should be 0-9."
     exit
+fi
+
+switch_prog pst -s swp0 --state=4
+switch_prog pst -s swp1 --state=0
+switch_prog pst -s swp2 --state=0
+if [ $2 -ne 0 ]; then
+    devmem 0x80078048 32 0xA
+    switch_prog ale -s -l 1 -t 300
+    switch_prog ale -s -l 1 -u
+    echo "##### Address learning Enabled. #####"
+else
+    echo "##### Address learning Disabled. #####"
+fi
+switch_prog pst -s swp1 --state=1
+switch_prog pst -s swp2 --state=1
+
+bridge=`ifconfig |grep br0 |grep -v grep`
+if [ -z "$bridge" ]; then
+    if [ $3 -ne 0 ]; then
+        ip link add name br0 type bridge
+        ip link set dev eth1 master br0
+        ip link set dev eth2 master br0
+        ifconfig br0 up
+        ip link set dev br0 type bridge stp_state 1
+        echo "##### STP Enabled. ######"
+    else
+        switch_prog pst -s swp1 --state=2
+        switch_prog pst -s swp2 --state=2
+        switch_prog pst -s swp1 --state=3
+        switch_prog pst -s swp2 --state=3
+        switch_prog pst -s swp1 --state=4
+        switch_prog pst -s swp2 --state=4
+        echo "##### STP Disabled. ######"
+    fi
+elif [ $3 -eq 0 ]; then
+    ip link set dev br0 type bridge stp_state 0
+    ifconfig br0 down
+    ip link delete dev br0 type bridge
+    switch_prog pst -s swp1 --state=2
+    switch_prog pst -s swp2 --state=2
+    switch_prog pst -s swp1 --state=3
+    switch_prog pst -s swp2 --state=3
+    switch_prog pst -s swp1 --state=4
+    switch_prog pst -s swp2 --state=4
+    echo "##### STP Disabled. ######"
 fi
 
 tadma_prog ep off
